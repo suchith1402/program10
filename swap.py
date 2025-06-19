8ef37faaf20294e9120b5b9822b88d229bf509ca
suchith1402 <suchithharshini@gmail.com>
2025-06-19 15:46:33 +0530

    Create swap.py

---
+++ swap.py
@@ -0,0 +1,7 @@
+# Simple Python Program: Greeting the user
+
+# Ask the user to enter their name
+name = input("What is your name? ")
+
+# Greet the user
+print("Hello, " + name + "! Welcome to Python.")
